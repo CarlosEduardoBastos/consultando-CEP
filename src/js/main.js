1abd73407ec98561dd6c20c5bb91c9c3541896d2
dudu <dudu.lima.bastos@gmail.com>
2020-12-03 15:49:50 -0300

    preenchendo os campos automaticamente

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 const cep = document.querySelector('#cep');
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector('#'+campo)){
+           document.querySelector('#'+campo).value = result[campo]
+        }
+    }
+}
 cep.addEventListener('blur',(event)=>{
     let seach = cep.value.replace("-","");
     const options={
@@ -8,7 +15,7 @@ cep.addEventListener('blur',(event)=>{
     }
     fetch(`http://viacep.com.br/ws/${seach}/json/`,options)
     .then(response=>{response.json()
-        .then( data => console.log(data))
+        .then( data => showData(data))
     })
     .catch(event => console.log('Deu Erro: '+ event.message))
 })
