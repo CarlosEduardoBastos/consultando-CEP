2893e3cb3788d33825b0d4c16ac5eb78c2bb76c0
dudu <dudu.lima.bastos@gmail.com>
2020-12-03 15:36:38 -0300

    pegando os dados json

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 const cep = document.querySelector('#cep');
 cep.addEventListener('blur',(event)=>{
-    
-    console.log(cep.value)
+    let seach = cep.value.replace("-","");
+    const options={
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+    }
+    fetch(`http://viacep.com.br/ws/${seach}/json/`,options)
+    .then(response=>{response.json()
+        .then( data => console.log(data))
+    })
+    .catch(event => console.log('Deu Erro: '+ event.message))
 })
